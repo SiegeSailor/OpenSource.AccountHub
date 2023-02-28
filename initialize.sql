b5f5bca365878d10ddfab97b5e48426c8055f086
SiegeSailor <siegesailor@gmail.com>
2023-02-28 00:13:58 -0500

    Initialize project with start files

--- initialize.sql
+++ initialize.sql
@@ -1,15 +1,18 @@
+CREATE DATABASE account_hub;
+
 USE account_hub;
 
 CREATE TABLE account (
     email               VARCHAR(320)        PRIMARY KEY,
     username            VARCHAR(16)         NOT NULL,
-    hased_password      VARCHAR(320)        NOT NULL,
+    hashed_password     VARCHAR(320)        NOT NULL,
+    privilege_level     TINYINT             DEFAULT 0,
     created_at          TIMESTAMP           DEFAULT CURRENT_TIMESTAMP,
     updated_at          TIMESTAMP           DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE history (
-    identitfer          INT                 NOT NULL PRIMARY KEY,
+    identitfer          INT                 NOT NULL AUTO_INCREMENT,
     category            CHAR(1)             NOT NULL,
     title               VARCHAR(80)         NOT NULL,
     content             VARCHAR(320),
--- source/entry-point.js
+++ source/entry-point.js
@@ -1,1 +1,17 @@
-require("crypto");
+const express = require("express");
+
+const application = express();
+
+application.use(express.json());
+application.use(express.urlencoded({ extended: true }));
+
+application.use("/", require("./routes/index.js"));
+application.use(function (request, response) {
+  response
+    .status(404)
+    .send(`Route "${encodeURIC(request.url)}" doesn't exist.`);
+});
+
+application.listen(3000, function () {
+  console.log("http://localhost:3000");
+});
