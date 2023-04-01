8cc6454b7280c7d206342099fb9eeae82bc3818f
SiegeSailor <siegesailor@gmail.com>
2023-04-01 01:59:40 -0400

    Remove email field Expose 3306 in database Dockerfile

--- database/initialize.sql
+++ database/initialize.sql
@@ -3,8 +3,7 @@ CREATE DATABASE account_hub;
 USE account_hub;
 
 CREATE TABLE account (
-    email               VARCHAR(320)        PRIMARY KEY,
-    username            VARCHAR(16)         UNIQUE NOT NULL,
+    username            VARCHAR(320)         PRIMARY KEY,
     passcode            VARCHAR(320)        NOT NULL,
     salt                VARCHAR(256)        NOT NULL,
     nobility            TINYINT             DEFAULT 1,
@@ -17,8 +16,8 @@ CREATE TABLE history (
     identifier          INT                 NOT NULL AUTO_INCREMENT,
     category            CHAR(1)             NOT NULL,
     content             VARCHAR(80)         NOT NULL,
-    email               VARCHAR(320)        NOT NULL,
+    username            VARCHAR(320)        NOT NULL,
     created_at          TIMESTAMP           DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (email) REFERENCES account (email) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (username) REFERENCES account (username) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (identifier)
 );
